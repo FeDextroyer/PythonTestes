9e398d486f17f91ec564b456656394b40c16c6ab
Felipe <aloferreirafm@gmail.com>
2024-04-27 14:57:38 -0300

    hoje

--- Codes/Teste3.py
+++ Codes/Teste3.py
@@ -1,22 +1,17 @@
-import unittest
 from flask import Flask
 
 app = Flask(__name__)
 
 class Calculator:
-    @staticmethod
     def add(x, y):
         return x + y
 
-    @staticmethod
     def subtract(x, y):
         return x - y
 
-    @staticmethod
     def multiply(x, y):
         return x * y
 
-    @staticmethod
     def divide(x, y):
         if y == 0:
             raise ValueError("Cannot divide by zero")
